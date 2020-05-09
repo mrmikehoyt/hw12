459c9cca4112835ad733e97bd0a676fd74127a86
Michael Hoyt <m.mikehoyt@outlook.com>
2020-05-08 21:21:11 -0400

    corrected syntax errors

--- seed.sql
+++ seed.sql
@@ -18,26 +18,26 @@ VALUES ("Jane","Doe",4,1);
 
 /*Seeding department database */
 INSERT INTO department (name)
-VALUES ("Sales")
+VALUES ("Sales");
 
 INSERT INTO department (name)
-VALUES ("Finance")
+VALUES ("Finance");
 
 INSERT INTO department (name)
-VALUES ("Legal")
+VALUES ("Legal");
 
 INSERT INTO department (name)
-VALUES ("Engineering")
+VALUES ("Engineering");
 
 /*Seeding role database */
 INSERT INTO role (title, salary, department_id)
-VALUES ("Sales Lead",100000,1)
+VALUES ("Sales Lead",100000,1);
 INSERT INTO role (title, salary, department_id)
-VALUES ("Software Engineer",120000,2)
+VALUES ("Software Engineer",120000,2);
 INSERT INTO role (title, salary, department_id)
-VALUES ("Sales Engineer",500000,3)
+VALUES ("Sales Engineer",500000,3);
 INSERT INTO role (title, salary, department_id)
-VALUES ("VP",1000000,4)
+VALUES ("VP",1000000,4);
 
 /*NOt sure if this belongs in schema or seeds. This is a join command to join the department, employee, and role tables so that data can be queryed on all 3 databases. */
 select * from department LEFT JOIN  role ON role.department_id  RIGHT JOIN employee ON employee.role_id
