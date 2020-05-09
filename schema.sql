ff5df03c73d924563a1fbdd19a002879285951b6
Michael Hoyt <m.mikehoyt@outlook.com>
2020-05-09 10:25:53 -0400

    corrected case

--- schema.sql
+++ schema.sql
@@ -1,33 +1,33 @@
 /*copy below in mysql workbench and run that*/
 /*Drops database if exists*/
-Drop Database if exists CompanyDB;
+DROP DATABASE IF EXISTS companydb;
 /*Creates database if exists*/
-Create Database CompanyDB;
-use CompanyDB;
+CREATE DATABASE companydb;
+USE companydb;
 /*Creates employee table and creates columns with datatypes, character lengths, and set id to not null*/
-Create Table employee(
+CREATE TABLE employee(
 id INTEGER NOT NULL AUTO_INCREMENT,
-first_name varchar (30) not null,
-last_name varchar (30) not null,
-role_id integer (99),
-manager_id integer (99),
+first_name VARCHAR (30) NOT NULL,
+last_name VARCHAR (30) NOT NULL,
+role_id INTEGER (99),
+manager_id INTEGER (99),
 PRIMARY KEY (id),
 FOREIGN KEY (role_id) references employee(id),
 FOREIGN KEY (manager_id) references employee(id)
 );
 /*Creates role table and creates columns with datatypes, decimal positions, character lengths, and set id to not null*/
-CREATE Table role(
+CREATE TABLE role(
 id INTEGER NOT NULL AUTO_INCREMENT,
-title varchar (30) not null,
-salary decimal (9,2),
-department_id integer (99),
+title VARCHAR (30) not null,
+salary DECIMAL (9,2),
+department_id INTEGER (99),
 PRIMARY KEY (id),
 FOREIGN KEY (department_id) references role(id)
 );
 /*Creates department table, name column, character length, and set id to not null*/
-create table department(
+CREATE TABLE department(
 id INTEGER NOT NULL AUTO_INCREMENT,
-name varchar (30) not null,
+name VARCHAR (30) NOT NULL,
 PRIMARY KEY (id)
 );
 /*copy above in mysql workbench and run that, below is used in Employee.js or notes */
