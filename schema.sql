3ad54b9663e9bf65ba534b54b6842b4708228973
Michael Hoyt <m.mikehoyt@outlook.com>
2020-05-08 20:16:46 -0400

    updated schema.sql, corrected columns, added comments

--- schema.sql
+++ schema.sql
@@ -1,16 +1,18 @@
+/*Drops database if exists*/
 Drop Database if exists CompanyDB;
+/*Creates database if exists*/
 Create Database CompanyDB;
 use CompanyDB;
+/*Creates employee table and creates columns with datatypes, character lengths, and set id to not null*/
 Create Table employee (
-  id INTEGER NOT NULL AUTO_INCREMENT,
-  plan VARCHAR(255) NOT NULL,
+id INTEGER NOT NULL AUTO_INCREMENT,
 first_name varchar (30) not null,
 last_name varchar (30) not null,
 role_id integer (99),
 manager_id integer (99),
 PRIMARY KEY (id)
 );
-
+/*Creates role table and creates columns with datatypes, decimal positions, character lengths, and set id to not null*/
 CREATE Table role(
 id INTEGER NOT NULL AUTO_INCREMENT,
 title varchar (30) not null,
@@ -18,7 +20,7 @@ salary decimal (9,2),
 department_id integer (99),
 PRIMARY KEY (id)
 );
-
+/*Creates department table, name column, character length, and set id to not null*/
 create table department(
 id INTEGER NOT NULL AUTO_INCREMENT,
 name varchar (30) not null,
